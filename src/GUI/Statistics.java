90e0066b3cb06292ccfcab3bdcf0e594a48458ea
yahia <yahia@YAYA-PC.esprit.tn>
2014-03-07 13:31:34 +0100

    Statistics Done

--- src/GUI/Statistics.java
+++ src/GUI/Statistics.java
@@ -8,6 +8,9 @@ package GUI;
 
 import UTIL.MyConnection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -18,9 +21,53 @@ public class Statistics {
     int MessagesNumber;
     int RoadNumber;
     public Statistics(){
-        String requete;
-   /*         requete = "select count(*) from users";
+        UsersNumber=0;
+        MessagesNumber=0;
+        RoadNumber=0;
+    }
+    
+    public int getUsersNumber(){
+        int number=0;
+        String requete= "select count(*) from user";
         try {
-            PreparedStatement ps = MyConnection.getInstance().prepareStatement(requete);
-   */ }
+            Statement statement = MyConnection.getInstance().createStatement();
+            ResultSet resultat = statement.executeQuery(requete);
+            while(resultat.next()){
+                number=resultat.getInt(1);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error in user statistics "+ex.getMessage());
+        }
+        return number;
+    }
+    
+    public int getInboxNumber(){
+        int number=0;
+        String requete= "select count(*) from message";
+        try {
+            Statement statement = MyConnection.getInstance().createStatement();
+            ResultSet resultat = statement.executeQuery(requete);
+            while(resultat.next()){
+                number=resultat.getInt(1);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error in message statistics "+ex.getMessage());
+        }
+        return number;
+    }
+    
+    public int getRoadNumber(){
+        int number=0;
+        String requete= "select count(*) from road";
+        try {
+            Statement statement = MyConnection.getInstance().createStatement();
+            ResultSet resultat = statement.executeQuery(requete);
+            while(resultat.next()){
+                number=resultat.getInt(1);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error in road statistics "+ex.getMessage());
+        }
+        return number;
+    }
 }
